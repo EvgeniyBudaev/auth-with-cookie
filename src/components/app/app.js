2a5b38f599cb9d03224750f870cdb9a86b4fc2ef
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2020-03-16 14:58:04 +0300

    finish

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from "react";
 import {
   Switch,
   Route,
   Redirect,
-  Link,
   BrowserRouter as Router
 } from "react-router-dom";
-// import Login from '../login';
-// import Dashboard from '../dashboard';
-import AuthApi from '../auth-api/auth-api';
-import Cookies from 'js-cookie';
+import AuthApi from "../auth-api/auth-api";
+import Cookies from "js-cookie";
+import TestartURL from "./testart-url";
 
 const App = () => {
   const [auth, setAuth] = React.useState(false);
@@ -19,11 +17,11 @@ const App = () => {
     if (user) {
       setAuth(true);
     }
-  }
+  };
 
   React.useEffect(() => {
     readCookie();
-  }, [])
+  }, []);
 
   return (
     <div>
@@ -33,80 +31,72 @@ const App = () => {
         </Router>
       </AuthApi.Provider>
     </div>
-  )
-}
+  );
+};
 
 const Login = () => {
-
   const Auth = React.useContext(AuthApi);
 
   const handlerOnClick = () => {
     Auth.setAuth(true);
-    Cookies.set("user", "loginTrue")
-  }
+    Cookies.remove("ulogin_token", "ue0bed434f6e522c4c35e7e5141473101");
+    Cookies.remove("_csrf");
+    Cookies.set(
+      "_identity",
+      "b1063fa405e89a118a33cc3f8544c92dce0bb4f6a3d57835b5b165141acb9506a%3A2%3A%7Bi%3A0%3Bs%3A9%3A%22_identity%22%3Bi%3A1%3Bs%3A50%3A%22%5B56674%2C%227SeJn00FyrFNictZcnC9ygfC4uAs4gi5%22%2C2592000%5D%22%3B%7D"
+    );
+  };
   return (
     <div>
       <h1>Авторизация с cookie</h1>
       <button onClick={handlerOnClick}>Войти</button>
     </div>
-  )
-}
+  );
+};
 
 const Dashboard = () => {
-
   const Auth = React.useContext(AuthApi);
 
   const handlerOnClick = () => {
     Auth.setAuth(false);
-    Cookies.remove("user");
-  }
+    Cookies.remove("_identity");
+    Cookies.remove("_csrf");
+  };
 
   return (
     <div>
       <h1>Dashboard</h1>
       <button onClick={handlerOnClick}>Выйти</button>
     </div>
-  )
-}
+  );
+};
 
 const Routes = () => {
   const Auth = React.useContext(AuthApi);
   return (
     <Switch>
-      <ProtectedLogin path="/login" auth={Auth.auth} component={Login} />
-      <ProtectedRoute path="/dashboard" auth={Auth.auth} component={Dashboard} />
+      <ProtectedLogin path="/auth" auth={Auth.auth} component={Login} exact />
+      <ProtectedRoute path="*" auth={Auth.auth} component={TestartURL} exact />
     </Switch>
-  )
-}
+  );
+};
 
 const ProtectedRoute = ({ auth, component: Component, ...rest }) => {
   return (
     <Route
       {...rest}
-      render={() => auth ? (
-      <Component />
-    ) :
-        (
-          <Redirect to="/login" />
-  )
-}
+      render={() => (auth ? <Component /> : <Redirect to="/auth" />)}
     />
-  )
-}
+  );
+};
 
 const ProtectedLogin = ({ auth, component: Component, ...rest }) => {
   return (
     <Route
       {...rest}
-      render={() => !auth ? (
-        <Component />
-      ) :
-        (
-          <Redirect to="/dashboard" />
-        )
-      }
+      render={() => (!auth ? <Component /> : <Redirect to="/" />)}
     />
-  )
-}
+  );
+};
 
 export default App;
